a8a977a1b8af319b71805a6e425536e366dfabd0
azatprojazz <azatprojazz@gmail.com>
2023-07-05 06:20:54 +0300

    feat: реализован компонент входа с полями e-mail и пароль

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import AuthPage from '../AuthPage/AuthPage';
+import './Login.css';
+
+function Login({ onSubmit }) {
+  const [email, setEmail] = useState('pochta@yandex.ru');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => setEmail(event.target.value);
+  const handlePasswordChange = (event) => setPassword(event.target.value);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit({ email, password });
+  };
+
+  return (
+    <main className="login">
+      <AuthPage
+        title="Рады видеть!"
+        btnText="Войти"
+        questionText="Ещё не зарегистрированы?"
+        url="/signup"
+        linkText="Регистрация"
+        name="login"
+        onSubmit={handleSubmit}
+        place="login"
+        autorize="login"
+      >
+        <label htmlFor="email" className="login__input-label">
+          E-mail
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={handleEmailChange}
+          className="login__input"
+          autoComplete="off"
+          required
+        />
+        <span className="login__input-error"></span>
+
+        <label htmlFor="password" className="login__input-label">
+          Пароль
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          minLength="6"
+          maxLength="30"
+          value={password}
+          onChange={handlePasswordChange}
+          className="login__input login__input_type_password"
+          autoComplete="off"
+          required
+        />
+        <span className="login__input-error"></span>
+      </AuthPage>
+    </main>
+  );
+}
+
+export default Login;
