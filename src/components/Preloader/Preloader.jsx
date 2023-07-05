2f08a3066e1a96f36a50cb8f13e56c268117d33c
azatprojazz <azatprojazz@gmail.com>
2023-07-05 06:22:02 +0300

    feat: добавлен компонент Preloader, требуется дальнейшая доработка

---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Preloader.css';
+const Preloader = () => {
+  return (
+    <div className="preloader">
+      <div className="preloader__container">
+        <span className="preloader__round"></span>
+      </div>
+    </div>
+  );
+};
+
+export default Preloader;
