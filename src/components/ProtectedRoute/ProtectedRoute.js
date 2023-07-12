4557bcc0e72ffc9511c4cf47b3d5592c0e1d28f4
azatprojazz <azatprojazz@gmail.com>
2023-07-12 21:17:22 +0300

    feat: реализация защищенного маршрута для авторизованных пользователей

---
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+// Компонент для защиты маршрутов, доступных только для авторизованных пользователей
+// Принимает другой компонент в качестве пропса и может передать ему неограниченное число пропсов
+const ProtectedRoute = ({ component: Component, ...props }) => {
+  // Если пользователь авторизован, рендерит переданный компонент, иначе перенаправляет на страницу входа
+  return props.loggedIn ? <Component {...props} /> : <Navigate to="/" replace />;
+};
+
+export default ProtectedRoute;
