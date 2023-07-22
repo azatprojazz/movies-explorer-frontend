1c9d1fe6bcb840d0a2a0fa0cfcb851907ccc0c80
azatprojazz <azatprojazz@gmail.com>
2023-07-22 19:10:29 +0300

    fix: добавлены обработчики для закрытия оверлея в Overlay

--- src/components/Overlay/Overlay.jsx
+++ src/components/Overlay/Overlay.jsx
@@ -1,7 +1,31 @@
 import './Overlay.css';
 
-function Overlay({ isOpen, children }) {
-  return <div className={`overlay ${isOpen ? 'overlay_is_opened' : ''}`}>{children}</div>;
+import { useEffect } from 'react';
+
+function Overlay({ isOpen, onClose, children }) {
+  const handleClickByOverlay = (e) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyEsc = (e) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+    isOpen && document.addEventListener('keydown', handleKeyEsc);
+    return () => {
+      document.removeEventListener('keydown', handleKeyEsc);
+    };
+  }, [isOpen, onClose]);
+
+  return (
+    <div className={`overlay ${isOpen ? 'overlay_is_opened' : ''}`} onClick={handleClickByOverlay}>
+      {children}
+    </div>
+  );
 }
 
 export default Overlay;
