94739136ad6d981073bf7c5855a421fd1ac2266b
azatprojazz <azatprojazz@gmail.com>
2023-07-22 19:37:46 +0300

    fix: перенести управление состоянием бургер-меню в компонент Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,30 @@
-import { useLocation } from 'react-router-dom';
 import './Header.css';
-import Navigation from '../Navigation/Navigation';
-import Logo from '../Logo/Logo';
-import BurgerMenuOpen from '../BurgerMenuOpen/BurgerMenuOpen';
-import BurgerMenu from '../BurgerMenu/BurgerMenu';
 
-function Header({ isLoggedIn, isBurgerOpen, onOpenBurger, onCloseBurger }) {
+import BurgerMenu from '../BurgerMenu/BurgerMenu';
+import BurgerMenuOpen from '../BurgerMenuOpen/BurgerMenuOpen';
+import Logo from '../Logo/Logo';
+import Navigation from '../Navigation/Navigation';
+import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
+
+function Header({ isLoggedIn }) {
+  const [isBurgerOpen, setIsBurgerOpen] = useState(false);
+
+  // функция для открытия бургер-меню
+  const handleOpenBurger = () => {
+    setIsBurgerOpen(true);
+  };
+
+  // функция для закрытия бургер-меню
+  const handleCloseBurger = () => {
+    setIsBurgerOpen(false);
+  };
+
   // Использование хука useLocation для получения текущего пути (pathname)
   const { pathname } = useLocation();
 
-  // Создание класса для компонента header, зависящего от текущего маршрута.
+ // Создание класса для компонента header, зависящего от текущего маршрута.
+  // Если путь совпадает с одним из указанных, добавляется специальный класс 'header_path_movies'
   const headerClass = `header ${
     pathname === '/movies' || pathname === '/saved-movies' || pathname === '/profile'
       ? 'header_path_movies'
@@ -21,8 +36,8 @@ function Header({ isLoggedIn, isBurgerOpen, onOpenBurger, onCloseBurger }) {
       <div className="header__container section-container">
         <Logo />
         <Navigation isLoggedIn={isLoggedIn} />
-        <BurgerMenuOpen onOpenBurger={onOpenBurger} isLoggedIn={isLoggedIn} />
-        <BurgerMenu isOpen={isBurgerOpen} onClose={onCloseBurger} />
+        <BurgerMenuOpen onOpenBurger={handleOpenBurger} isLoggedIn={isLoggedIn} />
+        <BurgerMenu isOpen={isBurgerOpen} onClose={handleCloseBurger} />
       </div>
     </header>
   );
