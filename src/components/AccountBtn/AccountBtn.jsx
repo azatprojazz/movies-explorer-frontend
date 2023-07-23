cd15b172d1d29887a7017e6ef1ce1ac1f8c63456
azatprojazz <azatprojazz@gmail.com>
2023-07-23 15:52:57 +0300

    fix: исправить клик по кнопке аккаунта с телефона

--- src/components/AccountBtn/AccountBtn.jsx
+++ src/components/AccountBtn/AccountBtn.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 function AccountBtn({ onClose }) {
   return (
-    <Link className="account-btn" to="/profile" onClick={onClose && onClose}>
+    <Link className="account-btn" to="/profile" onClick={onClose ?? undefined}>
       Аккаунт
     </Link>
   );
